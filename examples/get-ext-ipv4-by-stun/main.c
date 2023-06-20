f849692cc46e58827d0277089b5f407c1b22caed
nothingIIersonal <magomedov.r.m@edu.mirea.ru>
2023-06-21 01:38:56 +0300

    first tools

---
+++ examples/get-ext-ipv4-by-stun/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+
+#include <nettools/nettools.h>
+
+
+#define STUN_DOMAIN "stun2.l.google.com"
+#define STUN_IP "74.125.204.127"
+#define STUN_PORT 19302
+
+
+int main(int argc, char *argv[])
+{
+	struct ipv4_t ext_ipv4 = { 0 };
+	port_t local_port = 0;
+
+	{ // using STUN domain name
+		printf("Using STUN domain name \"%s\":\n", STUN_DOMAIN);
+
+		struct ip_t ip;
+		memset(&ip, 0, sizeof(ip));
+
+		int res = get_ip_by_domain_name(STUN_DOMAIN, &ip, 1);
+		chperror(res, -1, false, "get_ip_by_domain_name()");
+
+		uint8_t stun_ip[16];
+		memset(stun_ip, 0, sizeof(stun_ip));
+
+		sprintf(stun_ip, "%" PRIu8 ".%" PRIu8 ".%" PRIu8 ".%" PRIu8, ip.ipv4.A, ip.ipv4.B, ip.ipv4.C, ip.ipv4.D);
+
+		res = get_ext_ipv4_by_stun(stun_ip, STUN_PORT, &ext_ipv4, &local_port);
+		chperror(res, 0, true, "get_ext_ipv4()");
+
+		printf("\tYour external IPv4 and local opened PORT is \"%" PRIu8 ".%" PRIu8
+															".%" PRIu8 ".%" PRIu8 ":%" PRIu16 "\"\n",
+															ext_ipv4.A, ext_ipv4.B, ext_ipv4.C, ext_ipv4.D, local_port);
+	}
+
+	{ // using STUN IPv4
+		printf("Using STUN IPv4 address \"%s\":\n", STUN_IP);
+
+		int res = get_ext_ipv4_by_stun(STUN_IP, STUN_PORT, &ext_ipv4, &local_port);
+		chperror(res, 0, true, "get_ext_ipv4()");
+
+		printf("\tYour external IPv4 and local opened PORT is \"%" PRIu8 ".%" PRIu8
+															".%" PRIu8 ".%" PRIu8 ":%" PRIu16 "\"\n",
+															ext_ipv4.A, ext_ipv4.B, ext_ipv4.C, ext_ipv4.D, local_port);
+	}
+
+	return 0;
+}
---
+++ examples/get-ip-by-domain-name/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+#include <nettools/nettools.h>
+
+
+#define MAX_IPS 16
+#define DOMAIN_NAME "stun2.l.google.com"
+
+
+int main(int argc, char *argv[])
+{
+	struct ip_t ips[MAX_IPS];
+	memset(ips, 0, sizeof(ips));
+
+	int res = get_ip_by_domain_name(DOMAIN_NAME, ips, MAX_IPS);
+	chperror(res, -1, false, "get_ip_by_domain_name()");
+
+	if ( !res )
+		printf("No IP found.\n");
+	else
+		for ( int i = 0; i < res; ++i )
+		{
+			if ( !ips[i].type )
+				printf("IPv4 of \"%s\" is \"%" PRIu8 ".%" PRIu8 ".%" PRIu8 ".%" PRIu8 "\"\n",
+						DOMAIN_NAME, ips[i].ipv4.A, ips[i].ipv4.B, ips[i].ipv4.C, ips[i].ipv4.D);
+			else
+				printf("IPv6 of \"%s\" is \"%.4" PRIx16 ":%.4" PRIx16 ":%.4" PRIx16 ":%.4" PRIx16
+										  ":%.4" PRIx16 ":%.4" PRIx16 ":%.4" PRIx16 ":%.4" PRIx16 "\"\n",
+						DOMAIN_NAME, ips[i].ipv6.A, ips[i].ipv6.B, ips[i].ipv6.C, ips[i].ipv6.D,
+										ips[i].ipv6.E, ips[i].ipv6.F, ips[i].ipv6.G, ips[i].ipv6.H);
+		}
+
+	return 0;
+}
